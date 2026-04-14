1c88df6f8dd21d3e7b17ce16a8beaea637f2ab53
Hassnain-cs <mhassnain3547@conestogac.on.ca>
2026-04-13 21:20:47 -0400

    Updated KNNClassifier.cpp

---
+++ Phone-Orientation-Detector/KNNClassifier.cpp
@@ -0,0 +1,33 @@
+// KNNClassifier.cpp
+// Stub implementation for K-Nearest Neighbor classifier
+
+#include "KNNClassifier.h"
+#include <iostream>
+
+KNNClassifier::KNNClassifier() {
+}
+
+KNNClassifier::~KNNClassifier() {
+}
+
+void KNNClassifier::train(const std::vector<double>& xValues,
+    const std::vector<double>& yValues,
+    const std::vector<double>& zValues,
+    const std::vector<int>& labels) {
+    std::cout << std::endl;
+    std::cout << "KNNClassifier::train() was called" << std::endl;
+    std::cout << "This classifier is not implemented yet." << std::endl;
+    std::cout << "Please select the Nearest Neighbor classifier instead." << std::endl;
+}
+
+int KNNClassifier::classify(double x, double y, double z) {
+    std::cout << std::endl;
+    std::cout << "KNNClassifier::classify() was called" << std::endl;
+    std::cout << "This classifier is not implemented yet." << std::endl;
+    std::cout << "Please select the Nearest Neighbor classifier instead." << std::endl;
+    return -1;
+}
+
+std::string KNNClassifier::getClassifierName() const {
+    return "K-Nearest Neighbor Classifier (Not Implemented)";
+}
