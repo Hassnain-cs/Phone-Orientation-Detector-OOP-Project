a9b33feba3412cb996e021e200baa1e29a8f50ef
Hassnain-cs <mhassnain3547@conestogac.on.ca>
2026-04-13 21:12:57 -0400

    Updated KNNClassifier.h

--- Phone-Orientation-Detector/KNNClassifier.h
+++ Phone-Orientation-Detector/KNNClassifier.h
@@ -1,1 +1,33 @@
 #pragma once
+// KNNClassifier.h
+// K-Nearest Neighbor classifier stub
+// Another placeholder for future extension
+
+#ifndef KNNCLASSIFIER_H
+#define KNNCLASSIFIER_H
+
+#include "Classifier.h"
+#include <string>
+
+class KNNClassifier : public Classifier {
+public:
+    // Constructor
+    KNNClassifier();
+
+    // Destructor
+    ~KNNClassifier();
+
+    // Stub implementation of train function
+    void train(const std::vector<double>& xValues,
+        const std::vector<double>& yValues,
+        const std::vector<double>& zValues,
+        const std::vector<int>& labels) override;
+
+    // Stub implementation of classify function
+    int classify(double x, double y, double z) override;
+
+    // Returns the name of this classifier
+    std::string getClassifierName() const override;
+};
+
+#endif
